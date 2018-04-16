fbcab61372b2e0e06903aff5f7fcc16725bcf484
Matthew A. Pearon <mpearon@Pearon.Household>
2018-04-15 21:17:39 -0500

    Added missing directories

--- install.ps1
+++ install.ps1
@@ -1,4 +1,5 @@
 $mapGUIDirectory = New-Item (-join($env:APPDATA,'\mapGUI\')) -ItemType Directory
+
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 Invoke-WebRequest -Uri 'https://github.com/mpearon/mapGUI/archive/master.zip' -OutFile (-join($mapGUIDirectory,'\mapGUI-Source.zip'))
 Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -9,11 +10,18 @@ Get-ChildItem (-join($mapGUIDirectory,'\mapGUI-master')) | ForEach-Object{
 }
 Remove-Item (-join($mapGUIDirectory,'\mapGUI-master')) -Force -Recurse -Confirm:$false
 
+'\customizations\modules\','\operational\logs\' | ForEach-Object{
+    New-Item (-join($mapGUIDirectory,$_)) -ItemType Directory -Force
+}
+
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mpearon/PowerShell/master/Personal/StoryUp/StoryUpGUI/customizations/modules/storyUp.psm1' -OutFile (-join($mapGUIDirectory,'\customizations\modules\storyUp.psm1'))
+
 $customizationFile = Get-Item (-join($env:APPDATA,'\mapGUI\customizations\defaults.json'))
 $customizationFileContents = Get-Content $customizationFile | ConvertFrom-Json
 $customizationFileContents.workspace.applicationinfo.applicationRoot = ($customizationFile.Directory.Parent).FullName
 (-join('[',($customizationFileContents | ConvertTo-Json),']')) | Out-File $customizationFile
- 
+
 $AppLocation = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
 $arguments = (-join($env:APPDATA,'\mapGUI\mapGUI.ps1'))
 $WshShell = New-Object -ComObject WScript.Shell
