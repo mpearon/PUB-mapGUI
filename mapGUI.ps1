29fc2aefb2a484b4808e6de4234ad177ccce3aec
Pearon <Matthew.Pearon@centralbank.net>
2018-04-23 12:11:15 -0500

    Updated logic in update scriptblock to  exit update script on completion

--- mapGUI.ps1
+++ mapGUI.ps1
@@ -184,9 +184,7 @@ $masterRunspaceCode = {
     }
 
     $btn_configure_update.Add_Click({
-        #$codeBlock = 'Start-Sleep -Seconds 5; (-join($($env:APPDATA),"\mapGUI\install.ps1")'
-        #Start-Process PowerShell -ArgumentList $codeBlock
-        Start-Process PowerShell -ArgumentList "-noExit -Command `"&{ Start-Sleep -Seconds 5; & (-join(`$env:APPDATA,'\mapGUI\install.ps1'))}`""
+        Start-Process PowerShell -ArgumentList "-Command `"&{ Write-Host -ForegroundColor Red 'UPDATE IN PROGRESS'; & (-join(`$env:APPDATA,'\mapGUI\install.ps1'))}`""
         $syncHash.Window.Close()
         Stop-Process $pid
     })
