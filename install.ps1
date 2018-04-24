07c6f86c879b753c8ffa7edede625c9aee802352
Pearon <Matthew.Pearon@centralbank.net>
2018-04-24 10:41:06 -0500

    Adjusted install to look to latest release page for link

--- install.ps1
+++ install.ps1
@@ -12,7 +12,9 @@ else{
 }
 
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-Invoke-WebRequest -Uri 'https://github.com/mpearon/mapGUI/archive/master.zip' -OutFile (-join($mapGUIDirectory,'\mapGUI-Source.zip'))
+$latestRelease = (Invoke-WebRequest -uri 'https://github.com/mpearon/mapGUI/releases/latest').links | Where-Object { $_.outerText -match '(zip)' }
+#Invoke-WebRequest -Uri 'https://github.com/mpearon/mapGUI/archive/master.zip' -OutFile (-join($mapGUIDirectory,'\mapGUI-Source.zip'))
+Invoke-WebRequest -Uri (-join('https://github.com',$latestRelease)) -OutFile (-join($mapGUIDirectory,'\mapGUI-Source.zip'))
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory((-join($mapGUIDirectory,'\mapGUI-Source.zip')), $mapGUIDirectory)
 Remove-Item (-join($mapGUIDirectory,'\mapGUI-Source.zip')) -Force -Recurse -Confirm:$false
